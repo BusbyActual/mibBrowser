40664fd123c67533187b80a077a87fb76c8df881
busbyactual <busbyactual@gmail.com>
2017-08-31 08:47:14 -0700

    Added body parser to send args to method

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const mibs = require('./libsmiNew');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.post('/mibs', function (req, res) {
-//example
-  mibs.mibLoader(['RS-XX9-AIR-COOLING-MIB', 'RS-XX9-ATSC-MIB'])
-  res.send('Hello World!')
+  const body = req.body
+  mibs.mibLoader(body.mibs)
+  res.send(`You sent: ${body.mibs}`)
 })
+
+// app.post('/sms', function (req, res) {
+//   const body = req.body
+//   res.set('Content-Type', 'text/plain')
+//   res.send(`You sent: ${body.href} to Express`)
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
