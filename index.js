183e92c787fcdcb168985a886e7093f992c99b3c
busbyactual <busbyactual@gmail.com>
2017-10-03 10:12:39 -0700

    commented out test code and features in progress

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 const CircularJSON = require('circular-json');
-const cors = require('cors')
-var sails = require('sails');
-var multer  = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
+const cors = require('cors');
 
-var upload = multer({ storage: storage });
+// var sails = require('sails');
+// var multer  = require('multer');
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, '/uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// })
+// var upload = multer({ storage: storage });
 // const _ = require('underscore');
 // const path = require('path');
 // const upload = multer();
+
 const app = express()
 
 // parse application/x-www-form-urlencoded
@@ -29,7 +30,7 @@ app.use(bodyParser.json())
 
 
 // parse files
-app.use(require('skipper')());
+// app.use(require('skipper')());
 app.use(cors());
 
 
@@ -45,19 +46,19 @@ app.post('/mibs', function (req, res) {
   let data = mibs.getData();
   let children = mibs.getChildren(body.oid, data);
 
-  //mibs.formatSubroutine(data);
 
-  fs.writeFileSync('test.txt', CircularJSON.stringify(data))
-  //res.send(`You sent: ${body.mibs} ` + JSON.stringify(data))
+  // fs.writeFileSync('test.txt', CircularJSON.stringify(data))
+  // res.send(`You sent: ${body.mibs} ` + JSON.stringify(data))
+
   res.send(JSON.stringify(children))
 })
 
 
-app.post('/upload', upload.array(), function(req, res, next) {
+// app.post('/upload', upload.array(), function(req, res, next) {
  
 
   //let data  = [{'Node':'rsRoot','address':'1.3.6.1.4.1.2566','SmiDecl':'SMI_DECL_MODULEIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':null,'Format':null,'children':[{'Node':'rsCommon','address':'1.3.6.1.4.1.2566.113','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for common object and event definitions','Format':null,'children':[],'size':8},{'Node':'rsProducts','address':'1.3.6.1.4.1.2566.127','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for products.','Format':null,'children':[{'Node':'rsProdBroadcast','address':'1.3.6.1.4.1.2566.127.1','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for broadcast products.','Format':null,'children':[{'Node':'rsProdBroadcastMeasurement','address':'1.3.6.1.4.1.2566.127.1.1','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for broadcast measurement products.','Format':null,'children':[],'size':10},{'Node':'rsProdBroadcastTransmitter','address':'1.3.6.1.4.1.2566.127.1.2','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for broadcast transmitter products.','Format':null,'children':[],'size':10},{'Node':'rsProdBroadcastHeadend','address':'1.3.6.1.4.1.2566.127.1.3','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for broadcast headend products.','Format':null,'children':[],'size':10}],'size':9},{'Node':'rsProdRadioCommunications','address':'1.3.6.1.4.1.2566.127.2','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for radio communication products.','Format':null,'children':[{'Node':'rsProdRadioCommCommon','address':'1.3.6.1.4.1.2566.127.2.1','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for radio communication product common features.','Format':null,'children':[],'size':10},{'Node':'rsProdRadioCommSeries4200','address':'1.3.6.1.4.1.2566.127.2.2','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for radio communication series 4200.','Format':null,'children':[],'size':10},{'Node':'rsProdRadioCommSystems','address':'1.3.6.1.4.1.2566.127.2.3','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for radio communication systems.','Format':null,'children':[],'size':10},{'Node':'rsProdRadioCommEquipment','address':'1.3.6.1.4.1.2566.127.2.4','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for radio communication equipment.','Format':null,'children':[],'size':10}],'size':9}],'size':8},{'Node':'rsRequirements','address':'1.3.6.1.4.1.2566.131','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for management application requirements','Format':null,'children':[],'size':8},{'Node':'rsExperimental','address':'1.3.6.1.4.1.2566.137','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for experimental definitions','Format':null,'children':[],'size':8},{'Node':'rsCapabilities','address':'1.3.6.1.4.1.2566.139','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for agent capabilities','Format':null,'children':[],'size':8},{'Node':'rsRegistration','address':'1.3.6.1.4.1.2566.149','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for registrations','Format':null,'children':[{'Node':'rsRegModules','address':'1.3.6.1.4.1.2566.149.1','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for modules registrations','Format':null,'children':[],'size':9},{'Node':'rsRegBroadcast','address':'1.3.6.1.4.1.2566.149.2','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Description.','Format':null,'children':[{'Node':'rsRegBroadcastMeasurement','address':'1.3.6.1.4.1.2566.149.2.1','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Description.','Format':null,'children':[],'size':10},{'Node':'rsRegBroadcastTransmitter','address':'1.3.6.1.4.1.2566.149.2.2','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Description.','Format':null,'children':[],'size':10}],'size':9}],'size':8},{'Node':'rsCrypto','address':'1.3.6.1.4.1.2566.151','SmiDecl':'SMI_DECL_OBJECTIDENTITY','SmiAccess':'SMI_ACCESS_UNKNOWN','SmiStatus':'SMI_STATUS_CURRENT','SmiNodekind':'SMI_NODEKIND_NODE','Description':'Sub-tree for crypto algorithms','Format':null,'children':[],'size':8}],'size':7}];
-  console.log(req.body);
+  // console.log(req.body);
  //  var uploader = req.file();
  //  var config = {
  //     dirname: require('path').resolve('C:\\Data\\Projects\\libsmi\\uploads')
@@ -92,7 +93,7 @@ app.post('/upload', upload.array(), function(req, res, next) {
 // }
 // res.send(data);
 
-})
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
